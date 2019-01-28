c0bce2bf758a5a5b274a16ad36c631942b209013
GravyPouch <29843812+GravyPouch@users.noreply.github.com>
2019-01-27 21:37:00 -0500

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -3,41 +3,43 @@ package projecto;
 import java.util.*;
 
 public class Alphabet {
+	//Sets some varibles to 0 for use later
 	int x = 0;
 	int y = 0;
 	int z = 0;
+	//String that contains the alphabet
 	String other = new String ("abcdefghijklmnopqrstuvwxyz");
 	
 	void whatsMissing(){
 		System.out.println("Give me a word");
         Scanner console = new Scanner(System.in);
+		//prombts user for text
         String suckytext = console.nextLine();
+		//converts it into lowercase
         String text = suckytext.toLowerCase();
+		//prints the new lowercase text
 		System.out.println(text);
+		//checks the legth of the users text
 		int length = text.length();
 		System.out.println("length is "+length);
 		System.out.println("----------------");
 		System.out.println("The text does not contain: ");
+		//Starts a for loop to interate on the y variable.
+		//It ends at 25 becuase that is how long the alphabet is.
 		for (int y=0; y <= 25; y++) {
+			//converts the letter to a string
 			String letter = Character.toString(other.charAt(y));
+			//checks if the user's text contains the letter
 		if (text.contains(letter)) {
-	
-			
-			
+			//If it does. It does a whole lot of nothing
 		}
 		else{
+			//If it doesn't it prints the letter
 			System.out.print(letter);
-			
-
 		}
-		
 	}
 	}
-	
-
-	
-		
-
+//this just runs the script
 	public static void main(String[] args) {
 		
 		Alphabet run = new Alphabet();
